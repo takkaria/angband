76996a85b3ac388b6e9bb862476229141e0235e4
Vic K <t4nk074@openmailbox.org>
2016-07-15 11:25:02 +0800

    Implement ui2-term.c
    And rename some other files.
    Nothing works yet, of course.

---
+++ src/ui2-term.c
@@ -0,0 +1,846 @@
+/**
+ * \file ui-term.c
+ * \brief A generic, efficient, terminal window package
+ *
+ * Copyright (c) 1997 Ben Harrison and others
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ */
+
+#include "ui2-term.h"
+#include "z-virt.h"
+#include "z-util.c"
+
+/* Local declarations */
+
+struct term_cursor {
+	int x;
+	int y;
+	bool visible;
+};
+
+struct term_dirty {
+	int top;
+	int bottom;
+	struct {
+		int left;
+		int right;
+	} *rows;
+};
+
+struct term {
+	void *user;
+
+	bool temporary;
+
+	struct term_point *points;
+
+	int size;
+	int width;
+	int height;
+
+	struct {
+		struct term_cursor new;
+		struct term_cursor old;
+	} cursor;
+	struct term_dirty dirty;
+	struct term_callbacks callbacks;
+	struct term_point blank;
+};
+
+/* Global variables */
+
+/* TODO move that to ui2-display.c? */
+struct angband_user_terms angband_user_terms;
+term angband_term_cave;
+term angband_term_message_line;
+term angband_term_status_line;
+
+/* Local variables */
+
+#define TERM_STACK_MAX \
+	128
+
+#define TERM_KEY_QUEUE_MAX \
+	1024
+
+#define TOP \
+	(term_stack.stack[term_stack.top])
+
+#define INITIAL_TOP \
+	(-1)
+
+#define COORDS_OK(x, y) do { \
+	assert(TOP != NULL); \
+	assert((x) >= 0); \
+	assert((x) < TOP->width); \
+	assert((y) >= 0); \
+	assert((y) < TOP->height); \
+} while (0)
+
+#define STACK_OK() do { \
+	assert(TOP != NULL); \
+	assert(term_stack.top > INITIAL_TOP); \
+	assert(term_stack.top < (int) N_ELEMENTS(term_stack.stack)); \
+} while (0)
+
+/* cursor's x position can be one point beyound the right edge of the term */
+#define CURSOR_OK() do { \
+	assert(TOP->cursor.new.x >= 0); \
+	assert(TOP->cursor.new.x <= TOP->width); \
+	assert(TOP->cursor.new.y >= 0); \
+	assert(TOP->cursor.new.y < TOP->height); \
+} while (0)
+
+#define INDEX(x, y) \
+	((x) + (y) * TOP->width)
+
+#define POINT(x, y) \
+	(TOP->points[INDEX(x, y)])
+
+#define WIDESTRING_MAX \
+	1024
+
+static struct {
+	term stack[TERM_STACK_MAX];
+	int top;
+} term_stack = {
+	.top = INITIAL_TOP
+};
+
+static struct {
+	ui_event queue[TERM_KEY_QUEUE_MAX];
+	size_t head;
+	size_t tail;
+	size_t size;
+	size_t number;
+} event_queue = {
+	.size = TERM_KEY_QUEUE_MAX
+};
+
+/* Local functions */
+
+static void term_make_dirty(term t)
+{
+	t->dirty.top = 0;
+	t->dirty.bottom = t->height - 1;
+	for (int y = t->dirty.top; y <= t->dirty.bottom; y++) {
+		t->dirty.rows[y].left = 0;
+		t->dirty.rows[y].right = t->width - 1;
+	}
+
+	for (int i = 0; i < t->size; i++) {
+		t->points[i].dirty = true;
+	}
+}
+
+static term term_alloc(int w, int h)
+{
+	assert(w > 0);
+	assert(h > 0);
+
+	struct term *t = mem_zalloc(sizeof(*t));
+
+	t->width = w;
+	t->height = h;
+	t->size = w * h;
+
+	t->points = mem_zalloc(t->size * sizeof(t->points[0]));
+	t->dirty.rows = mem_zalloc(t->height * sizeof(t->dirty.rows[0]));
+
+	return t;
+}
+
+static void term_free(term t)
+{
+	mem_free(t->points);
+	mem_free(t->dirty.rows);
+	mem_free(t);
+}
+
+static void term_copy(term new, term old)
+{
+	int min_width = MIN(new->width, old->width);
+	int min_height = MIN(new->height, old->height);
+
+	for (int y = 0; y < min_height; y++) {
+		int newx = new->width * y;
+		int oldx = old->width * y;
+		for (int x = 0; x < min_width; x++, newx++, oldx++) {
+			new->points[newx] = old->points[oldx];
+		}
+	}
+}
+
+static term term_new(const struct term_create_info *info)
+{
+	term t = term_alloc(info->width, info->height);
+
+	t->user = info->user;
+	t->blank = info->blank;
+	t->callbacks = info->callbacks;
+
+	assert(t->callbacks.max_size != NULL);
+	assert(t->callbacks.push_new != NULL);
+	assert(t->callbacks.pop_new != NULL);
+	assert(t->callbacks.redraw != NULL);
+	assert(t->callbacks.cursor != NULL);
+	assert(t->callbacks.event != NULL);
+	assert(t->callbacks.delay != NULL);
+	assert(t->callbacks.draw != NULL);
+
+	term_make_dirty(t);
+
+	t->temporary = false;
+	
+	return t;
+}
+
+static void term_draw(int x, int y, int len)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+	assert(len > 0);
+
+	TOP->callbacks.draw(TOP->user, x, y, len, &POINT(x, y));
+}
+
+static void term_mark_point_dirty(int x, int y)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+
+	POINT(x, y).dirty = true;
+
+	if (y < TOP->dirty.top) {
+		TOP->dirty.top = y;
+	}
+	if (y > TOP->dirty.bottom) {
+		TOP->dirty.bottom = y;
+	}
+
+	if (x < TOP->dirty.rows[y].left) {
+		TOP->dirty.rows[y].left = x;
+	}
+	if (x > TOP->dirty.rows[y].right) {
+		TOP->dirty.rows[y].right = x;
+	}
+}
+
+static void term_mark_line_dirty(int x, int y, int len)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+	assert(len > 0);
+
+	int z = MIN(x + len, TOP->width);
+
+	term_mark_point_dirty(x, y);
+	while (x < z) {
+		POINT(x, y).dirty = true;
+		x++;
+	}
+	term_mark_point_dirty(z - 1, y);
+}
+
+static void term_mark_row_flushed(int y)
+{
+	STACK_OK();
+	COORDS_OK(TOP->dirty.rows[y].left, y);
+	COORDS_OK(TOP->dirty.rows[y].right, y);
+
+	for (int x = TOP->dirty.rows[y].left; x <= TOP->dirty.rows[y].right; x++) {
+		POINT(x, y).dirty = false;
+	}
+
+	TOP->dirty.rows[y].left = TOP->width;
+	TOP->dirty.rows[y].right = 0;
+}
+
+static struct term_point term_make_point(uint32_t fga, wchar_t fgc,
+		uint32_t bga, wchar_t bgc, bitflag *flags)
+{
+	struct term_point point = {
+		.dirty = true,
+		.fg_char = fgc,
+		.fg_attr = fga,
+		.bg_char = bgc,
+		.bg_attr = bga
+	};
+	
+	if (flags != NULL) {
+		tpf_copy(point.flags, flags);
+	}
+
+	return point;
+}
+
+static void term_set_point(int x, int y, struct term_point point)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+
+	POINT(x, y) = point;
+	term_mark_point_dirty(x, y);
+}
+
+static void term_set_fg(int x, int y, uint32_t fga, wchar_t fgc)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+
+	POINT(x, y).fg_attr = fga;
+	POINT(x, y).fg_char = fgc;
+	term_mark_point_dirty(x, y);
+}
+
+static bool term_put_point_at_cursor(struct term_point point)
+{
+	STACK_OK();
+	CURSOR_OK();
+	
+	if (TOP->cursor.new.x < TOP->width) {
+		term_set_point(TOP->cursor.new.x, TOP->cursor.new.y, point);
+		TOP->cursor.new.x++;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static bool term_put_fg_at_cursor(uint32_t fga, wchar_t fgc)
+{
+	STACK_OK();
+	CURSOR_OK();
+	
+	if (TOP->cursor.new.x < TOP->width) {
+		term_set_fg(TOP->cursor.new.x, TOP->cursor.new.y, fga, fgc);
+		TOP->cursor.new.x++;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static int term_add_ws(int x, int y, uint32_t fga, const wchar_t *ws)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+
+	assert(*ws != 0);
+
+	const wchar_t *curws = ws;
+
+	for (int curx = x; *curws != 0 && curx < TOP->width; curx++, curws++) {
+		POINT(curx, y).fg_attr = fga;
+		POINT(curx, y).fg_char = *curws;
+	}
+
+	term_mark_line_dirty(x, y, curws - ws);
+
+	return curws - ws;
+}
+
+static bool term_put_ws_at_cursor(uint32_t fga, const wchar_t *ws)
+{
+	STACK_OK();
+	CURSOR_OK();
+
+	TOP->cursor.new.x +=
+		term_add_ws(TOP->cursor.new.x, TOP->cursor.new.y, fga, ws);
+
+	CURSOR_OK();
+
+	return TOP->cursor.new.x < TOP->width;
+}
+
+static void term_wipe_line(int x, int y, int len)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+	assert(len > 0);
+
+	for (int curx = x, z = MIN(x + len, TOP->width); curx < z; curx++) {
+		POINT(curx, y) = TOP->blank;
+	}
+	term_mark_line_dirty(x, y, len);
+}
+
+static void term_move_cursor(int x, int y)
+{
+	STACK_OK();
+	CURSOR_OK();
+
+	TOP->cursor.new.x = x;
+	TOP->cursor.new.y = y;
+
+	CURSOR_OK();
+}
+
+static bool term_check_event(ui_event *event)
+{
+	if (event_queue.number == 0) {
+		return false;
+	}
+
+	if (event != NULL) {
+		*event = event_queue.queue[event_queue.tail];
+	}
+
+	return true;
+}
+
+static bool term_take_event(ui_event *event)
+{
+	if (!term_check_event(event)) {
+		return false;
+	}
+
+	assert(event_queue.number > 0);
+
+	event_queue.number--;
+	event_queue.tail++;
+
+	if (event_queue.tail == event_queue.size) {
+		event_queue.tail = 0;
+	}
+
+	return true;
+}
+
+static bool term_append_event(ui_event event)
+{
+	if (event_queue.number == event_queue.size) {
+		return false;
+	}
+
+	event_queue.queue[event_queue.head] = event;
+	event_queue.number++;
+	event_queue.head++;
+
+	if (event_queue.head == event_queue.size) {
+		event_queue.head = 0;
+	}
+
+	return true;
+}
+
+static void term_mbstowcs(wchar_t *ws, const char *mbs, size_t ws_max_len)
+{
+	size_t len = text_mbstowcs(ws, mbs, ws_max_len);
+
+	if (len == (size_t) -1) {
+		quit_fmt("can't convert the string '%s'", mbs);
+	} else if (len == ws_max_len) {
+		ws[ws_max_len - 1] = 0;
+	}
+}
+
+static void term_erase_cursor(struct term_cursor cursor)
+{
+	if (cursor.visible && cursor.x < TOP->width) {
+		term_draw(cursor.x, cursor.y, 1);
+	}
+}
+
+static void term_draw_cursor(struct term_cursor cursor)
+{
+	if (cursor.visible && cursor.x < TOP->width) {
+		TOP->callbacks.cursor(TOP->user, cursor.x, cursor.y);
+	}
+}
+
+static void term_flush_row(int y)
+{
+	STACK_OK();
+	COORDS_OK(TOP->dirty.rows[y].left, y);
+	COORDS_OK(TOP->dirty.rows[y].right, y);
+
+	int len = 0;
+	int drawx = 0;
+
+	for (int x = TOP->dirty.rows[y].left; x <= TOP->dirty.rows[y].right; x++) {
+		if (POINT(x, y).dirty) {
+			if (len == 0) {
+				drawx = x;
+			}
+			len++;
+		} else if (len > 0) {
+			term_draw(drawx, y, len);
+			len = 0;
+		}
+	}
+
+	if (len > 0) {
+		term_draw(drawx, y, len);
+	}
+
+	term_mark_row_flushed(y);
+}
+
+static void term_flush(void)
+{
+	for (int y = TOP->dirty.top; y <= TOP->dirty.bottom; y++) {
+		term_flush_row(y);
+	}
+
+	TOP->dirty.top = TOP->height;
+	TOP->dirty.bottom = 0;
+}
+
+/* External functions */
+
+term Term_create(const struct term_create_info *info)
+{
+	return term_new(info);
+}
+
+void Term_destroy(term *t)
+{
+	assert((*t)->temporary == false);
+
+	term_free(*t);
+	*t = NULL;
+}
+
+void Term_setpriv(term t, void *user)
+{
+	t->user = user;
+}
+
+void *Term_getpriv(term t)
+{
+	return t->user;
+}
+
+term Term_top(void)
+{
+	return TOP;
+}
+
+void Term_push(term t)
+{
+	assert(t != NULL);
+	assert(!t->temporary);
+
+	term_stack.top++;
+	TOP = t;
+
+	STACK_OK();
+}
+
+void Term_push_new(struct term_hints *hints)
+{
+	assert(hints->width > 0);
+	assert(hints->height > 0);
+
+	struct term_create_info info = {0};
+
+	TOP->callbacks.push_new(*hints, &info);
+
+	assert(info.width == 0 || info.width == hints->width);
+	assert(info.height == 0 || info.height == hints->height);
+
+	if (info.width == 0) {
+		info.width = hints->width;
+	}
+	if (info.height == 0) {
+		info.height = hints->height;
+	}
+
+	term_stack.top++;
+	TOP = term_new(&info);
+	TOP->temporary = true;
+
+	STACK_OK();
+}
+
+void Term_pop(void)
+{
+	if (TOP->temporary) {
+		TOP->callbacks.pop_new(TOP->user);
+		term_free(TOP);
+	}
+
+	TOP = NULL;
+	term_stack.top--;
+
+	STACK_OK();
+}
+
+void Term_addwchar(int x, int y,
+		uint32_t fga, wchar_t fgc, uint32_t bga, wchar_t bgc, bitflag *flags)
+{
+	term_set_point(x, y, term_make_point(fga, fgc, bga, bgc, flags));
+}
+
+void Term_addwc(int x, int y, uint32_t fga, wchar_t fgc)
+{
+	term_set_fg(x, y, fga, fgc);
+}
+
+void Term_addws(int x, int y, uint32_t fga, const wchar_t *fgc)
+{
+	(void) term_add_ws(x, y, fga, fgc);
+}
+
+void Term_adds(int x, int y, uint32_t fga, const char *fgc)
+{
+	wchar_t ws[WIDESTRING_MAX];
+	term_mbstowcs(ws, fgc, WIDESTRING_MAX);
+
+	(void) term_add_ws(x, y, fga, ws);
+}
+
+bool Term_putwchar(int x, int y,
+		uint32_t fga, wchar_t fgc, uint32_t bga, wchar_t bgc, bitflag *flags)
+{
+	term_move_cursor(x, y);
+
+	return term_put_point_at_cursor(term_make_point(fga, fgc, bga, bgc, flags));
+}
+
+bool Term_putwc(int x, int y, uint32_t fga, wchar_t fgc)
+{
+	term_move_cursor(x, y);
+
+	return term_put_fg_at_cursor(fga, fgc);
+}
+
+bool Term_putws(int x, int y, uint32_t fga, const wchar_t *fgc)
+{
+	term_move_cursor(x, y);
+
+	return term_put_ws_at_cursor(fga, fgc);
+}
+
+bool Term_puts(int x, int y, uint32_t fga, const char *fgc)
+{
+	term_move_cursor(x, y);
+
+	wchar_t ws[WIDESTRING_MAX];
+	term_mbstowcs(ws, fgc, WIDESTRING_MAX);
+
+	return term_put_ws_at_cursor(fga, ws);
+}
+
+void Term_erase(int x, int y)
+{
+	term_wipe_line(x, y, TOP->width);
+}
+
+void Term_erase_length(int x, int y, int len)
+{
+	term_wipe_line(x, y, len);
+}
+
+void Term_clear(void)
+{
+	for (int y = 0; y < TOP->height; y++) {
+		term_wipe_line(0, y, TOP->width);
+	}
+}
+
+void Term_dirty_point(int x, int y)
+{
+	term_mark_point_dirty(x, y);
+}
+
+void Term_dirty_region(int left, int top, int right, int bottom)
+{
+	COORDS_OK(left, top);
+	COORDS_OK(right, bottom);
+
+	assert(left <= right);
+	assert(top <= bottom);
+
+	int len = right - left + 1;
+	for (int y = top; y <= bottom; y++) {
+		term_mark_line_dirty(left, y, len);
+	}
+}
+
+void Term_dirty_all(void)
+{
+	for (int y = 0; y < TOP->height; y++) {
+		term_mark_line_dirty(0, y, TOP->width);
+	}
+}
+
+void Term_get_cursor(int *x, int *y, bool *visible, bool *usable)
+{
+	STACK_OK();
+	CURSOR_OK();
+
+	if (x != NULL) {
+		*x = TOP->cursor.new.x;
+	}
+	if (y != NULL) {
+		*y = TOP->cursor.new.y;
+	}
+	if (visible != NULL) {
+		*visible = TOP->cursor.new.visible;
+	}
+	if (usable != NULL) {
+		*usable = TOP->cursor.new.x < TOP->width;
+	}
+}
+
+void Term_cursor_to_xy(int x, int y)
+{
+	term_move_cursor(x, y);
+}
+
+void Term_cursor_visible(bool visible)
+{
+	TOP->cursor.new.visible = visible;
+}
+
+void Term_get_size(int *w, int *h)
+{
+	STACK_OK();
+
+	*w = TOP->width;
+	*h = TOP->height;
+}
+
+void Term_get_point(int x, int y, struct term_point *point)
+{
+	STACK_OK();
+	COORDS_OK(x, y);
+
+	*point = POINT(x, y);
+}
+
+void Term_add_point(int x, int y, struct term_point point)
+{
+	term_set_point(x, y, point);
+}
+
+bool Term_put_point(int x, int y, struct term_point point)
+{
+	term_move_cursor(x, y);
+
+	return term_put_point_at_cursor(point);
+}
+
+void Term_resize(int w, int h)
+{
+	STACK_OK();
+	assert(w > 0);
+	assert(h > 0);
+
+	struct term old = *TOP;
+
+	TOP->width = w;
+	TOP->height = h;
+	TOP->size = w * h;
+
+	TOP->points = mem_zalloc(TOP->size * sizeof(TOP->points[0]));
+	TOP->dirty.rows = mem_zalloc(TOP->height * sizeof(TOP->dirty.rows[0]));
+
+	for (int y = 0; y < TOP->height; y++) {
+		term_wipe_line(0, y, TOP->width);
+	}
+
+	term_copy(TOP, &old);
+
+	mem_free(old.points);
+	mem_free(old.dirty.rows);
+}
+
+void Term_flush_output(void)
+{
+	term_erase_cursor(TOP->cursor.old);
+	term_flush();
+	term_draw_cursor(TOP->cursor.new);
+
+	TOP->cursor.old = TOP->cursor.new;
+}
+
+void Term_redraw_screen(void)
+{
+	STACK_OK();
+
+	TOP->callbacks.redraw(TOP->user);
+}
+
+bool Term_keypress(keycode_t key, byte mods)
+{
+	STACK_OK();
+
+	ui_event event = {
+		.key = {
+			.type = EVT_KBRD,
+			.code = key,
+			.mods = mods
+		}
+	};
+
+	return term_append_event(event);
+}
+
+bool Term_mousepress(int x, int y, int button, byte mods)
+{
+	STACK_OK();
+
+	ui_event event = {
+		.mouse = {
+			.type = EVT_MOUSE,
+			.x = x,
+			.y = y,
+			.button = button,
+			.mods = mods
+		}
+	};
+
+	return term_append_event(event);
+}
+
+bool Term_take_event(ui_event *event, bool wait)
+{
+	STACK_OK();
+
+	assert(event != NULL);
+
+	if (event_queue.number == 0) {
+		TOP->callbacks.event(TOP->user, wait);
+	}
+
+	return term_take_event(event);
+}
+
+bool Term_check_event(ui_event *event)
+{
+	STACK_OK();
+
+	return term_check_event(event);
+}
+
+void Term_delay(int msecs)
+{
+	STACK_OK();
+
+	assert(msecs > 0);
+
+	TOP->callbacks.delay(TOP->user, msecs);
+}
+
+void Term_max_size(int *w, int *h)
+{
+	STACK_OK();
+
+	TOP->callbacks.max_size(TOP->user, w, h);
+
+	assert(*w > 0);
+	assert(*h > 0);
+}
---
+++ src/ui2-term.h
@@ -0,0 +1,309 @@
+/**
+ * \file ui2-term.h
+ *
+ * Copyright (c) 1997 Ben Harrison and others
+ *
+ * This work is free software; you can redistribute it and/or modify it
+ * under the terms of either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation, version 2, or
+ *
+ * b) the "Angband licence":
+ *    This software may be copied and distributed for educational, research,
+ *    and not for profit purposes provided that this copyright and statement
+ *    are included in all such copies.  Other copyrights may also apply.
+ */
+
+#ifndef INCLUDED_UI2_TERM_H
+#define INCLUDED_UI2_TERM_H
+
+#include "ui2-event.h"
+#include "h-basic.h"
+#include "z-bitflag.h"
+
+typedef struct term *term;
+
+enum term_point_flags {
+	TPF_NONE,
+	TPF_DUMMY,
+	TPF_MAX
+};
+
+#define TPF_SIZE                FLAG_SIZE(TPF_MAX)
+#define tpf_has(f, flag)        flag_has_dbg(f, TPF_SIZE, flag, #f, #flag)
+#define tpf_next(f, flag)       flag_next(f, TPF_SIZE, flag)
+#define tpf_is_empty(f)         flag_is_empty(f, TPF_SIZE)
+#define tpf_is_full(f)          flag_is_full(f, TPF_SIZE)
+#define tpf_is_inter(f1, f2)    flag_is_inter(f1, f2, TPF_SIZE)
+#define tpf_is_subset(f1, f2)   flag_is_subset(f1, f2, TPF_SIZE)
+#define tpf_is_equal(f1, f2)    flag_is_equal(f1, f2, TPF_SIZE)
+#define tpf_on(f, flag)         flag_on_dbg(f, TPF_SIZE, flag, #f, #flag)
+#define tpf_off(f, flag)        flag_off(f, TPF_SIZE, flag)
+#define tpf_wipe(f)             flag_wipe(f, TPF_SIZE)
+#define tpf_setall(f)           flag_setall(f, TPF_SIZE)
+#define tpf_negate(f)           flag_negate(f, TPF_SIZE)
+#define tpf_copy(f1, f2)        flag_copy(f1, f2, TPF_SIZE)
+#define tpf_union(f1, f2)       flag_union(f1, f2, TPF_SIZE)
+#define tpf_comp_union(f1, f2)  flag_comp_union(f1, f2, TPF_SIZE)
+#define tpf_inter(f1, f2)       flag_inter(f1, f2, TPF_SIZE)
+#define tpf_diff(f1, f2)        flag_diff(f1, f2, TPF_SIZE)
+
+struct term_point {
+	bool dirty;
+
+	/* foreground character and attribute */
+	uint32_t fg_attr;
+	wchar_t fg_char;
+
+	/* background character and attribute */
+	uint32_t bg_attr;
+	wchar_t bg_char;
+
+	bitflag flags[TPF_SIZE];
+};
+
+/*
+ * the terms above MUST be initialized at the start of the game
+ * by the frontend. Recommended placement of these terms:
+ * angband_term_cave in the center
+ * angband_term_message_line above angband_term_cave
+ * angband_term_status_line below angband_term_cave
+ * (see Angband sirca 4.0.5)
+ */
+extern term angband_term_cave;
+extern term angband_term_message_line;
+extern term angband_term_status_line;
+
+struct angband_user_terms {
+	term *term;
+	char *name;
+	size_t number;
+};
+
+/* what is displayed in these terms can be controlled by the user;
+ * it is recommended to create several of those */
+extern struct angband_user_terms angband_user_terms;
+
+/* Flags which determine what this term does */
+enum term_window_flags {
+	TWF_NONE, /* cave, message line and status line have no flags */
+	TWF_INVEN,
+	TWF_EQUIP,
+	TWF_PLAYER_BASIC,
+	TWF_PLAYER_EXTRA,
+	TWF_PLAYER_COMPACT,
+	TWF_MAP,
+	TWF_MESSAGE,
+	TWF_OVERHEAD,
+	TWF_MONSTER,
+	TWF_OBJECT,
+	TWF_MONLIST,
+	TWF_ITEMLIST,
+	TWF_MAX
+};
+
+#define TWF_SIZE                FLAG_SIZE(TWF_MAX)
+#define twf_has(f, flag)        flag_has_dbg(f, TWF_SIZE, flag, #f, #flag)
+#define twf_next(f, flag)       flag_next(f, TWF_SIZE, flag)
+#define twf_is_empty(f)         flag_is_empty(f, TWF_SIZE)
+#define twf_is_full(f)          flag_is_full(f, TWF_SIZE)
+#define twf_is_inter(f1, f2)    flag_is_inter(f1, f2, TWF_SIZE)
+#define twf_is_subset(f1, f2)   flag_is_subset(f1, f2, TWF_SIZE)
+#define twf_is_equal(f1, f2)    flag_is_equal(f1, f2, TWF_SIZE)
+#define twf_on(f, flag)         flag_on_dbg(f, TWF_SIZE, flag, #f, #flag)
+#define twf_off(f, flag)        flag_off(f, TWF_SIZE, flag)
+#define twf_wipe(f)             flag_wipe(f, TWF_SIZE)
+#define twf_setall(f)           flag_setall(f, TWF_SIZE)
+#define twf_negate(f)           flag_negate(f, TWF_SIZE)
+#define twf_copy(f1, f2)        flag_copy(f1, f2, TWF_SIZE)
+#define twf_union(f1, f2)       flag_union(f1, f2, TWF_SIZE)
+#define twf_comp_union(f1, f2)  flag_comp_union(f1, f2, TWF_SIZE)
+#define twf_inter(f1, f2)       flag_inter(f1, f2, TWF_SIZE)
+#define twf_diff(f1, f2)        flag_diff(f1, f2, TWF_SIZE)
+
+/* forward declaration */
+struct term_create_info;
+
+/* on push_new_hook and pop_new_hook
+ * push_new_hook is called when a new, temporary term is about to be
+ * pushed on the stack (with the call Term_push_new())
+ * the frontend must supply the desirable parameters for creation in term_create_info
+ * (taking term_hints into account as it sees fit; the only thing guaranteed is that
+ * the new term's width and height are equal to those specified in term_hints)
+ * and must be ready to draw on this new term
+ * pop_new_hook is called when the temporary term is about to be popped
+ * from the stack; the frontend must perform all necessary cleanup
+ *
+ * push_new_hook and pop_new_hook are mandatory
+ *
+ * note that the frontend shouldn't create or destroy temporary terms;
+ * that will be done without it */
+
+struct term_hints {
+	int width;
+	int height;
+};
+
+/* NOTE THAT ALL HOOKS ARE MANDATORY */
+
+typedef void (*push_new_hook)(struct term_hints hints,
+		struct term_create_info *info);
+typedef void (*pop_new_hook)(void *user);
+
+/* draw_hook should draw num_points points starting at position x, y */
+typedef void (*draw_hook)(void *user,
+		int x, int y, int num_points, struct term_point *points);
+/* max_size_hook should return the maximum width and height
+ * of a term that can be created */
+typedef void (*max_size_hook)(void *user, int *w, int *h);
+/* cursor_hook should draw a cursor at position x, y */
+typedef void (*cursor_hook)(void *user, int x, int y);
+/* redraw_hook should make sure that all that was drawn previously
+ * actually appears on the screen */
+typedef void (*redraw_hook)(void *user);
+/* event_hook should call Term_keypress() or Term_mousepress()
+ * if there are corresponding events; if wait is true, the frontend should
+ * wait for events (as long as necessary to get at least one) */
+typedef void (*event_hook)(void *user, bool wait);
+/* delay_hook should pause for specified number of milliseconds */
+typedef void (*delay_hook)(void *user, int msecs);
+
+struct term_callbacks {
+	push_new_hook push_new;
+	pop_new_hook pop_new;
+	draw_hook draw;
+	event_hook event;
+	delay_hook delay;
+	cursor_hook cursor;
+	redraw_hook redraw;
+	max_size_hook max_size;
+};
+
+struct term_create_info {
+	int width;
+	int height;
+	void *user;
+	struct term_callbacks callbacks;
+	struct term_point blank;
+};
+
+term Term_create(const struct term_create_info *info);
+void Term_destroy(term *t);
+
+
+void *Term_getpriv(term t);
+void Term_setpriv(term t, void *user);
+
+term Term_top(void);
+
+void Term_push(term t);
+void Term_pop(void);
+
+/* fga - foreground attr (usually color)
+ * fgc - foreground char
+ * bga - background attr (usually color)
+ * bgc - background char
+ * x and y must be valid 
+ * Term_add* are like Term_put* functions, but dont move the cursor */
+void Term_addwchar(int x, int y,
+		uint32_t fga, wchar_t fgc, uint32_t bga, wchar_t bgc, bitflag *flags);
+/* simplified interface to the above; background attr and char will be unchanged */
+void Term_addwc(int x, int y, uint32_t fga, wchar_t fgc);
+/* as above, but with a null-terminated string of wchar_t
+ * only the part of the string that fits in the term window will be used */
+void Term_addws(int x, int y, uint32_t fga, const wchar_t *fgc);
+/* as above, but with a (utf-8 encoded) string of chars */
+void Term_adds(int x, int y, uint32_t fga, const char *fgc);
+
+/* make a point "dirty" - it will be passed to draw_hook()
+ * at the time of flushing output */
+void Term_dirty_point(int x, int y);
+/* make points in a region dirty */
+void Term_dirty_region(int x, int y, int w, int h);
+/* make all points of the term dirty */
+void Term_dirty_all(void);
+
+/* erase row starting at x, y and ending with the edge of the term window
+ * these functions do not move the cursor */
+void Term_erase(int x, int y);
+/* erase some characters starting at x, y; but no more than len of them */
+void Term_erase_length(int x, int y, int len);
+/* wipe the whole window */
+void Term_clear(void);
+
+/* notes about the cursor
+ * cursor's X (horizontal) legal positions are (0 <= X <= term width)
+ * and Y (vertical) legal positions are (0 <= Y < term height).
+ * note that the cursor can be one point beyond the right edge of the term.
+ * All other positions are FATAL ERRORS */
+
+/* Term_put* functions move the cursor 
+ * they return true if all characters were added
+ * and return false if some could not be added
+ * because of the cursor's inability to advance from its current position
+ * (due to it being the last legal position) */
+bool Term_putwchar(int x, int y,
+		uint32_t fga, wchar_t fgc, uint32_t bga, wchar_t bgc, bitflag *flags);
+/* simplified interface to the above */
+bool Term_putwc(int x, int y, uint32_t fga, wchar_t fgc);
+/* as above, but with a null-terminated string of wchar_t */
+bool Term_putws(int x, int y, uint32_t fga, const wchar_t *fgc);
+/* as above, but with a (utf-8 encoded) string of chars */
+bool Term_puts(int x, int y, uint32_t fga, const char *fgc);
+
+/* determine the position and visibility of the cursor
+ * "usable" means that the cursor is not beyond the edge of the term window
+ * pass NULL for any argument that doesn't interest you */
+void Term_get_cursor(int *x, int *y, bool *visible, bool *usable);
+/* set the position of the cursor */
+void Term_cursor_to_xy(int x, int y);
+/* set visibility of the cursor (Term_put* functions will still
+ * move it as usual whether it is visible or not) */
+void Term_cursor_visible(bool visible);
+
+/* At a given location, determine the current point */
+void Term_get_point(int x, int y, struct term_point *point);
+/* add a point; dont move the cursor */
+void Term_add_point(int x, int y, struct term_point point);
+/* add a point; and move the cursor
+ * returns true if the point was added */
+bool Term_put_point(int x, int y, struct term_point point);
+
+/* determine the size of the top term */
+void Term_get_size(int *w, int *h);
+/* returns the maximum width and height that a term can have */
+void Term_max_size(int *w, int *h);
+/* make the top term bigger or smaller */
+void Term_resize(int w, int h);
+
+/* flush the output (it won't necessarily appear on the screen;
+ * that depends on how th frontend does it; what will happen is that
+ * draw_hook will be called) */
+void Term_flush_output(void);
+
+/* redraw the display; note that most monitors (at the time of writing this)
+ * can only be redrawn 60 times a second; trying to redraw more can cause
+ * flickering, tearing of frames, slowdowns and other undesirable things */
+void Term_redraw_screen(void);
+
+/* append a keypress to ui_event queue;
+ * returns false if queue is full and event cannot be added */
+bool Term_keypress(keycode_t key, byte mods);
+/* append a mousepress to ui_event queue
+ * returns false if queue is full and event cannot be added */
+bool Term_mousepress(int x, int y, int button, byte mods);
+
+/* get the first event from the event queue
+ * returns false if there are no events
+ * wait means wait as long as necessary for an event */
+bool Term_take_event(ui_event *event, bool wait);
+/* check if there is at least one event in the queue;
+ * the event will be copied to the first argument, but not removed
+ * from the queue; if you don't want the event, supply NULL as the argument */
+bool Term_check_event(ui_event *event);
+
+/* pause for some milliseconds */
+void Term_delay(int msecs);
+
+#endif /* INCLUDED_UI2_TERM_H */
