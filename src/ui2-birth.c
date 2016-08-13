ea7a8076691a66c9d85a381b22e8ad3a18132236
Vic K <t4nk074@openmailbox.org>
2016-08-13 14:09:50 +0800

    Make birth menu work

--- src/ui2-birth.c
+++ src/ui2-birth.c
@@ -142,26 +142,50 @@ static struct menu roller_menu;
 /**
  * Locations of the menus, etc. on the screen
  */
-#define HEADER_ROW          1
-#define QUESTION_ROW        7
-#define TABLE_ROW           9
+#define HEADER_ROW         1
+#define QUESTION_ROW       7
+#define BIRTH_MENU_ROW     9
+#define HISTORY_ROW       18
 
-#define QUESTION_COL        2
-#define RACE_COL            2
-#define RACE_AUX_COL       19
-#define CLASS_COL          19
-#define CLASS_AUX_COL      36
-#define ROLLER_COL         36
-#define HIST_INSTRUCT_ROW  18
+#define HISTORY_COL        1
+#define QUESTION_COL       2
+#define RACE_MENU_COL      2
+#define CLASS_MENU_COL    19
+#define ROLLER_MEU_COL    36
 
-#define MENU_ROWS          14
+#define RACE_MENU_WIDTH   17
+#define CLASS_MENU_WIDTH  17
+#define ROLLER_MENU_WIDTH 34
+
+#define HISTORY_WIDTH 71
+#define HISTORY_HEIGHT 3
+
+#define BIRTH_MENU_HEIGHT \
+	(ANGBAND_TERM_STANDARD_HEIGHT - BIRTH_MENU_ROW - 1)
 
 /**
  * upper left column and row, width, and lower column
  */
-static region race_region   = {RACE_COL,   TABLE_ROW, 17, MENU_ROWS};
-static region class_region  = {CLASS_COL,  TABLE_ROW, 17, MENU_ROWS};
-static region roller_region = {ROLLER_COL, TABLE_ROW, 34, MENU_ROWS};
+static region race_region = {
+	.x = RACE_MENU_COL,
+	.y = BIRTH_MENU_ROW,
+	.w = RACE_MENU_WIDTH,
+	.h = BIRTH_MENU_HEIGHT
+};
+
+static region class_region = {
+	.x = CLASS_MENU_COL,
+	.y = BIRTH_MENU_ROW,
+	.w = CLASS_MENU_WIDTH,
+	.h = BIRTH_MENU_HEIGHT
+};
+
+static region roller_region = {
+	.x = ROLLER_MEU_COL,
+	.y = BIRTH_MENU_ROW,
+	.w = ROLLER_MENU_WIDTH,
+	.h = BIRTH_MENU_HEIGHT
+};
 
 /**
  * We use different menu "browse functions" to display the help text
@@ -207,7 +231,7 @@ static const menu_iter birth_iter = {
 
 static void skill_help(struct text_out_info info,
 		const int *r_skills, const int *c_skills,
-		int mhp, int exp, int infra)
+		int mhp, int exp, int infra, int *rows)
 {
 	int skills[SKILL_MAX];
 
@@ -220,27 +244,37 @@ static void skill_help(struct text_out_info info,
 			skills[SKILL_TO_HIT_MELEE],
 			skills[SKILL_TO_HIT_BOW],
 			skills[SKILL_TO_HIT_THROW]);
+	(*rows)++;
+
 	text_out_e(info,
 			"Hit die: %2d   XP mod: %d%%\n",
 			mhp, exp);
+	(*rows)++;
+
 	text_out_e(info,
 			"Disarm: %+3d/%+3d   Devices: %+3d\n",
 			skills[SKILL_DISARM_PHYS],
 			skills[SKILL_DISARM_MAGIC],
 			skills[SKILL_DEVICE]);
+	(*rows)++;
+
 	text_out_e(info,
 			"Save:   %+3d   Stealth: %+3d\n",
 			skills[SKILL_SAVE],
 			skills[SKILL_STEALTH]);
+	(*rows)++;
 
 	if (infra >= 0) {
 		text_out_e(info, "Infravision:  %d ft\n", infra * 10);
+		(*rows)++;
 	}
 
 	text_out_e(info, "Digging:      %+d\n", skills[SKILL_DIGGING]);
+	(*rows)++;
 
 	if (infra < 0) {
 		text_out_e(info, "\n");
+		(*rows)++;
 	}
 }
 
@@ -286,7 +320,6 @@ static const char *get_pflag_desc(bitflag flag)
 static void race_help(int index, void *data, region reg)
 {
 	(void) data;
-	(void) reg;
 
 	struct player_race *race = player_id2race(index);
 	if (!race) {
@@ -294,10 +327,12 @@ static void race_help(int index, void *data, region reg)
 	}
 	
 	struct text_out_info info = {
-		.indent = RACE_AUX_COL
+		.indent = reg.x + reg.w
 	};
 
-	Term_cursor_to_xy(RACE_AUX_COL, TABLE_ROW);
+	Term_cursor_to_xy(reg.x + reg.w, reg.y);
+
+	int rows = 0;
 
 	for (int stat = 0, len = (STAT_MAX + 1) / 2; stat < len; stat++) {
 		const char *name = stat_names_reduced[stat];
@@ -312,46 +347,47 @@ static void race_help(int index, void *data, region reg)
 		}
 
 		text_out(info, "\n");
+		rows++;
 	}
 	
 	text_out_e(info, "\n");
-	skill_help(info, race->r_skills, NULL, race->r_mhp, race->r_exp, race->infra);
+	rows++;
+
+	skill_help(info, race->r_skills, NULL, race->r_mhp, race->r_exp, race->infra, &rows);
+
 	text_out_e(info, "\n");
+	rows++;
 
-	int n_flags = 0;
-	int flag_space = 3;
-
-	for (int flag = 0; flag < OF_MAX && n_flags < flag_space; flag++) {
+	for (int flag = 1; flag < OF_MAX && rows < reg.h; flag++) {
 		if (of_has(race->flags, flag)) {
 			text_out_e(info, "\n%s", get_flag_desc(flag));
-			n_flags++;
+			rows++;
 		}
 	}
 
-	for (int flag = 0; flag < ELEM_MAX && n_flags < flag_space; flag++) {
+	for (int flag = 1; flag < ELEM_MAX && rows < reg.h; flag++) {
 		if (race->el_info[flag].res_level == 1) {
 			text_out_e(info, "\n%s", get_resist_desc(flag));
-			n_flags++;
+			rows++;
 		}
 	}
 
-	for (int flag = 0; flag < PF_MAX && n_flags < flag_space; flag++) {
+	for (int flag = 1; flag < PF_MAX && rows < reg.h; flag++) {
 		if (pf_has(race->pflags, flag)) {
 			text_out_e(info, "\n%s", get_pflag_desc(flag));
-			n_flags++;
+			rows++;
 		}
 	}
 
-	while (n_flags < flag_space) {
+	while (rows < reg.h) {
 		text_out_e(info, "\n");
-		n_flags++;
+		rows++;
 	}
 }
 
 static void class_help(int index, void *data, region reg)
 {
 	(void) data;
-	(void) reg;
 
 	struct player_class *class = player_id2class(index);
 	if (!class) {
@@ -361,9 +397,12 @@ static void class_help(int index, void *data, region reg)
 	const struct player_race *race = player->race;
 
 	struct text_out_info info = {
-		.indent = CLASS_AUX_COL
+		.indent = reg.x + reg.w
 	};
-	Term_cursor_to_xy(CLASS_AUX_COL, TABLE_ROW);
+
+	Term_cursor_to_xy(reg.x + reg.w, reg.y);
+
+	int rows = 0;
 
 	for (int stat = 0, len = (STAT_MAX + 1) / 2; stat < len; stat++) {  
 		const char *name = stat_names_reduced[stat];
@@ -378,30 +417,31 @@ static void class_help(int index, void *data, region reg)
 		}
 
 		text_out(info, "\n");
+		rows++;
 	}
 
 	text_out_e(info, "\n");
+	rows++;
 	
 	skill_help(info, race->r_skills, class->c_skills,
-			race->r_mhp + class->c_mhp, race->r_exp + class->c_exp, -1);
+			race->r_mhp + class->c_mhp, race->r_exp + class->c_exp, -1, &rows);
 
-	if (class->magic.spell_realm->index != REALM_NONE)
+	if (class->magic.spell_realm->index != REALM_NONE) {
 		text_out_e(info, "\nLearns %s magic", class->magic.spell_realm->adjective);
+		rows++;
+	}
 
-	int n_flags = 0;
-	int flag_space = 5;
-
-	for (int flag = 0; flag < PF_MAX && n_flags < flag_space; flag++) {
+	for (int flag = 1; flag < PF_MAX && rows < reg.h; flag++) {
 		if (pf_has(class->pflags, flag)) {
 			const char *s = get_pflag_desc(flag);
 			text_out_e(info, "\n%s", s);
-			n_flags++;
+			rows++;
 		}
 	}
 
-	while (n_flags < flag_space) {
+	while (rows < reg.h) {
 		text_out_e(info, "\n");
-		n_flags++;
+		rows++;
 	}
 }
 
@@ -490,6 +530,8 @@ static void setup_menus(void)
 		"Standard roller" 
 	};
 
+	assert(N_ELEMENTS(roller_choices) == MAX_BIRTH_ROLLERS);
+
 	init_birth_menu(&roller_menu, MAX_BIRTH_ROLLERS, 0, roller_region, false, NULL);
 
 	struct birthmenu_data *roller_data = menu_priv(&roller_menu);
@@ -528,7 +570,7 @@ static void free_birth_menus(void)
  */
 static void clear_question(void)
 {
-	for (int y = QUESTION_ROW; y < TABLE_ROW; y++) {
+	for (int y = QUESTION_ROW; y < BIRTH_MENU_ROW; y++) {
 		Term_erase_line(0, y);
 	}
 }
@@ -560,12 +602,12 @@ static void print_menu_instructions(void)
  * corresponding command to the game.  Some actions are handled entirely
  * by the UI (displaying help text, for instance).
  */
-static enum birth_stage menu_question(enum birth_stage current,
+static enum birth_stage menu_question(enum birth_stage current_stage,
 		struct menu *current_menu, cmd_code choice_command)
 {
 	struct birthmenu_data *menu_data = menu_priv(current_menu);
 
-	enum birth_stage next = BIRTH_RESET;
+	enum birth_stage next_stage = BIRTH_RESET;
 	
 	/* Print the question currently being asked. */
 	clear_question();
@@ -574,7 +616,7 @@ static enum birth_stage menu_question(enum birth_stage current,
 
 	current_menu->stop_keys = "?=*\x18"; /* ?, =, *, Ctrl-X */
 
-	while (next == BIRTH_RESET) {
+	while (next_stage == BIRTH_RESET) {
 		/* Display the menu, wait for a selection of some sort to be made. */
 		ui_event event = menu_select(current_menu);
 
@@ -582,9 +624,9 @@ static enum birth_stage menu_question(enum birth_stage current,
 		   use of "back" (left arrow key or equivalent) to step back in 
 		   the proces as well as "escape". */
 		if (event.type == EVT_ESCAPE) {
-			next = BIRTH_BACK;
+			next_stage = BIRTH_BACK;
 		} else if (event.type == EVT_SELECT) {
-			if (current == BIRTH_ROLLER_CHOICE) {
+			if (current_stage == BIRTH_ROLLER_CHOICE) {
 				if (current_menu->cursor == BR_POINTBASED) {
 					/* 
 					 * Make sure we've got a point-based char to play with. 
@@ -598,17 +640,17 @@ static enum birth_stage menu_question(enum birth_stage current,
 					cmdq_push(CMD_RESET_STATS);
 					cmd_set_arg_choice(cmdq_peek(), "choice", true);
 					/* go to BIRTH_POINTBASED */
-					next = current + 1;
+					next_stage = current_stage + 1;
 				} else {
 					/* Do a first roll of the stats */
 					cmdq_push(CMD_ROLL_STATS);
 					/* go to BIRTH_ROLLER */
-					next = current + 2;
+					next_stage = current_stage + 2;
 				}
 			} else {
 				cmdq_push(choice_command);
 				cmd_set_arg_choice(cmdq_peek(), "choice", current_menu->cursor);
-				next = current + 1;
+				next_stage = current_stage + 1;
 			}
 		} else if (event.type == EVT_KBRD) {
 			/* '*' chooses an option at random from those the game's provided */
@@ -618,10 +660,10 @@ static enum birth_stage menu_question(enum birth_stage current,
 				cmd_set_arg_choice(cmdq_peek(), "choice", current_menu->cursor);
 
 				menu_refresh(current_menu);
-				next = current + 1;
+				next_stage = current_stage + 1;
 			} else if (event.key.code == '=') {
 				do_cmd_options_birth();
-				next = current;
+				next_stage = current_stage;
 			} else if (event.key.code == KTRL('X')) {
 				quit(NULL);
 			} else if (event.key.code == '?') {
@@ -630,7 +672,7 @@ static enum birth_stage menu_question(enum birth_stage current,
 		}
 	}
 	
-	return next;
+	return next_stage;
 }
 
 /**
@@ -649,7 +691,7 @@ static enum birth_stage roller_command(bool first_call)
 	static bool prev_roll = false;
 
 	/* Display the player - a bit cheaty, but never mind. */
-	display_player(0);
+	display_player(PLAYER_DISPLAY_MODE_STANDARD);
 
 	if (first_call) {
 		prev_roll = false;
@@ -760,6 +802,8 @@ static void point_based_points(game_event_type type,
 	
 	loc.x = TOTAL_COL;
 	put_str(format("Total Cost: %2d/%2d", sum, data->birthstats.remaining + sum), loc);
+
+	Term_flush_output();
 }
 
 static void point_based_start(void)
@@ -774,18 +818,22 @@ static void point_based_start(void)
 
 	show_prompt(prompt);
 
+	Term_cursor_visible(true);
+
 	/* Register handlers for various events - cheat a bit because we redraw
 	   the lot at once rather than each bit at a time. */
-	event_add_handler(EVENT_BIRTHPOINTS, point_based_points, NULL);	
-	event_add_handler(EVENT_STATS, point_based_stats, NULL);	
-	event_add_handler(EVENT_GOLD, point_based_misc, NULL);	
+	event_add_handler(EVENT_BIRTHPOINTS, point_based_points, NULL);
+	event_add_handler(EVENT_STATS, point_based_stats, NULL);
+	event_add_handler(EVENT_GOLD, point_based_misc, NULL);
 }
 
 static void point_based_stop(void)
 {
-	event_remove_handler(EVENT_BIRTHPOINTS, point_based_points, NULL);	
-	event_remove_handler(EVENT_STATS, point_based_stats, NULL);	
-	event_remove_handler(EVENT_GOLD, point_based_misc, NULL);	
+	event_remove_handler(EVENT_BIRTHPOINTS, point_based_points, NULL);
+	event_remove_handler(EVENT_STATS, point_based_stats, NULL);
+	event_remove_handler(EVENT_GOLD, point_based_misc, NULL);
+
+	Term_cursor_visible(false);
 }
 
 static enum birth_stage point_based_command(void)
@@ -796,6 +844,7 @@ static enum birth_stage point_based_command(void)
 
 	/* Place cursor just after cost of current stat */
 	Term_cursor_to_xy(COSTS_COL + 4, COSTS_ROW + stat);
+	Term_flush_output();
 
 	/* Get key */
 	struct keypress key = inkey_only_key();
@@ -867,53 +916,78 @@ void get_screen_loc(size_t cursor, int *x, int *y,
 		size_t n_lines, size_t *line_starts, size_t *line_lengths)
 {
 	for (size_t i = 0, lengths_so_far = 0; i < n_lines; i++) {
-		if (cursor >= line_starts[i]) {
-			if (cursor <= line_starts[i] + line_lengths[i]) {
-				*x = cursor - lengths_so_far;
-				*y = i;
-				break;
-			}
+		if (cursor >= line_starts[i]
+				&& cursor <= line_starts[i] + line_lengths[i])
+		{
+			*x = cursor - lengths_so_far;
+			*y = i;
+			break;
 		}
-		/* +1 for the space */
+
+		/* +1 for the newline */
 		lengths_so_far += line_lengths[i] + 1;
 	}
 }
 
-int edit_text(char *buffer, size_t buflen) {
-	const region area = {1, HIST_INSTRUCT_ROW + 1, 71, 5};
+/*
+ * This function returns true when the user presses Enter,
+ * and false when Escape is pressed.
+ */
+bool edit_text(char *buffer, size_t buflen) {
+	Term_cursor_visible(true);
+
+	const region history_region = {
+		.x = HISTORY_COL, 
+		/* +1 for empty line above history */
+		.y = HISTORY_ROW + 1,
+		.w = HISTORY_WIDTH,
+		/* +1 to account for overflows */
+		.h = HISTORY_HEIGHT + 1
+	};
+
+	size_t *line_starts = NULL;
+	size_t *line_lengths = NULL;
 
 	size_t length = strlen(buffer);
 	size_t cursor = 0;
 
+	bool retval = false;
 	bool done = false;
 
+	show_prompt("['ESC' to step back, 'Enter' to accept]");
+
 	while (!done) {
+		assert(cursor <= length);
+		assert(length < buflen);
+
 		textblock *tb = textblock_new();
 
 		/* Display on screen */
-		clear_from(HIST_INSTRUCT_ROW);
+		clear_from(HISTORY_ROW);
 		textblock_append(tb, buffer);
-		textui_textblock_place(tb, area, NULL);
-
-		size_t *line_starts = NULL;
-		size_t *line_lengths = NULL;
+		textui_textblock_place(tb, history_region, NULL);
 
 		size_t n_lines = textblock_calculate_lines(tb,
-				&line_starts, &line_lengths, area.w);
+				&line_starts, &line_lengths, history_region.w);
 
 		/* Set cursor to current editing position */
 		int x;
 		int y;
 		get_screen_loc(cursor, &x, &y, n_lines, line_starts, line_lengths);
-		Term_cursor_to_xy(1 + x, 19 + y);
+		Term_cursor_to_xy(x + history_region.x, y + history_region.y);
+
+		Term_flush_output();
 
 		struct keypress key = inkey_only_key();
 
 		switch (key.code) {
 			case ESCAPE:
-				return -1;
+				retval = false;
+				done = true;
+				break;
 
 			case KC_ENTER:
+				retval = true;
 				done = true;
 				break;
 
@@ -930,15 +1004,17 @@ int edit_text(char *buffer, size_t buflen) {
 				break;
 
 			case ARROW_DOWN: {
-				size_t add = line_lengths[y] + 1;
-				if (cursor + add < length) {
-					cursor += add;
+				 /* +1 to step over newline */
+				size_t down = line_lengths[y] + 1;
+				if (cursor + down < length) {
+					cursor += down;
 				}
 				break;
 			}
 
 			case ARROW_UP:
 				if (y > 0) {
+					 /* +1 to step over newline */
 					size_t up = line_lengths[y - 1] + 1;
 					if (cursor >= up) {
 						cursor -= up;
@@ -955,54 +1031,54 @@ int edit_text(char *buffer, size_t buflen) {
 				break;
 
 			case KC_BACKSPACE:
-			case KC_DELETE: {
-				/* Refuse to backspace into oblivion */
-				if ((key.code == KC_BACKSPACE && cursor == 0)
-						|| (key.code == KC_DELETE && cursor >= length))
-				{
-					break;
-				}
-
-				/* Move the string from k to nul along to the left by 1 */
-				if (key.code == KC_BACKSPACE) {
-					memmove(&buffer[cursor - 1], &buffer[cursor], length - cursor);
+				if (cursor > 0) {
+					/* Erase previous char (before the cursor) */
+					memmove(buffer + cursor - 1, buffer + cursor, length - cursor);
 					cursor--;
-				} else {
-					memmove(&buffer[cursor], &buffer[cursor + 1], length - cursor - 1);
+					length--;
+					buffer[length] = 0;
 				}
-				length--;
-				buffer[length] = 0;
 				break;
-			}
+
+			case KC_DELETE:
+				if (cursor < length) {
+					/* Erase char under the cursor */
+					memmove(buffer + cursor, buffer + cursor + 1, length - cursor - 1);
+					length--;
+					buffer[length] = 0;
+				}
+				break;
 			
 			default:
-				if (!isprint(key.code)) {
-					break;
+				if (isprint(key.code) && length + 1 < buflen) {
+					if (cursor < length) {
+						/* Cursor is not at the end of the string;
+						 * move the rest of the buffer along to make room */
+						memmove(buffer + cursor + 1, buffer + cursor, length - cursor);
+					}
+
+					buffer[cursor] = (char) key.code;
+					cursor++;
+					length++;
+					buffer[length] = 0;
 				}
-				if (buffer[cursor] == 0) {
-					/* Make sure we have enough room for a new character */
-					if (cursor + 1 >= buflen) break;
-				} else {
-					/* Make sure we have enough room to add a new character */
-					if (length + 1 >= buflen) break;
-
-					/* Move the rest of the buffer along to make room */
-					memmove(&buffer[cursor + 1], &buffer[cursor], length - cursor);
-				}
-
-				/* Insert the character */
-				buffer[cursor] = (char) key.code;
-				cursor++;
-				length++;
-				buffer[length] = 0;
-
 				break;
 		}
 
 		textblock_free(tb);
 	}
 
-	return 0;
+	if (line_starts != NULL) {
+		mem_free(line_starts);
+	}
+	if (line_lengths != NULL) {
+		mem_free(line_lengths);
+	}
+
+	Term_cursor_visible(false);
+	Term_flush_output();
+
+	return retval;
 }
 
 /**
@@ -1028,18 +1104,15 @@ static enum birth_stage get_history_command(void)
 	} else if (key.code == ESCAPE) {
 		next = BIRTH_BACK;
 	} else if (key.code == 'N' || key.code == 'n') {
-		char history[240];
+		char history[HISTORY_WIDTH * HISTORY_HEIGHT];
 		my_strcpy(history, player->history, sizeof(history));
 
-		switch (edit_text(history, sizeof(history))) {
-			case -1:
-				next = BIRTH_BACK; /* XXX fallthru? */
-
-			case 0:
-				cmdq_push(CMD_HISTORY_CHOICE);
-				cmd_set_arg_string(cmdq_peek(), "history", history);
-				next = BIRTH_HISTORY_CHOICE;
-				break;
+		if (edit_text(history, sizeof(history))) {
+			cmdq_push(CMD_HISTORY_CHOICE);
+			cmd_set_arg_string(cmdq_peek(), "history", history);
+			next = BIRTH_HISTORY_CHOICE;
+		} else {
+			next = BIRTH_BACK;
 		}
 	} else {
 		next = BIRTH_FINAL_CONFIRM;
@@ -1141,6 +1214,7 @@ int textui_do_birth(void)
 				command = CMD_CHOOSE_RACE;
 
 				Term_clear();
+				clear_prompt();
 				print_menu_instructions();
 
 				if (current > BIRTH_RACE_CHOICE) {
@@ -1247,8 +1321,8 @@ static void ui_enter_birthscreen(game_event_type type,
 	quickstart_allowed = data->flag;
 
 	struct term_hints hints = {
-		.width = 80,
-		.height = 24,
+		.width = ANGBAND_TERM_STANDARD_WIDTH,
+		.height = ANGBAND_TERM_STANDARD_HEIGHT,
 		.purpose = TERM_PURPOSE_BIRTH
 	};
 	Term_push_new(&hints);
@@ -1279,4 +1353,3 @@ void ui_init_birthstate_handlers(void)
 	event_add_handler(EVENT_ENTER_BIRTH, ui_enter_birthscreen, NULL);
 	event_add_handler(EVENT_LEAVE_BIRTH, ui_leave_birthscreen, NULL);
 }
-
