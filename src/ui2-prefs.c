9dc931e0049afa0c24e9a1dbc4505767413009ad
Vic K <t4nk074@openmailbox.org>
2016-08-20 19:49:39 +0800

    Use !streq() instead of strcmp()

--- src/ui2-prefs.c
+++ src/ui2-prefs.c
@@ -532,7 +532,7 @@ static enum parser_error parse_prefs_object(struct parser *p)
 		uint32_t attr = parser_getuint(p, "attr");
 		wchar_t ch = parser_getuint(p, "char");
 
-		if (strcmp(sval, "*")) {
+		if (!streq(sval, "*")) {
 			return PARSE_ERROR_UNRECOGNISED_SVAL;
 		}
 
