4e5eb2b3db3fb4ee8540aaf8841333bf1615f58a
Vic K <t4nk074@openmailbox.org>
2016-08-20 22:07:40 +0800

    Remove space between prompt and "[y/n]" in textui_get_check()
    Just like it was originally, since core uses it this way (with space
    appended to prompt already).

--- src/ui2-input.c
+++ src/ui2-input.c
@@ -568,7 +568,7 @@ int textui_get_quantity(const char *prompt, int max)
 bool textui_get_check(const char *prompt)
 {
 	char buf[80];
-	strnfmt(buf, sizeof(buf), "%.70s [y/n]", prompt);
+	strnfmt(buf, sizeof(buf), "%.70s[y/n]", prompt);
 
 	show_prompt(buf, false);
 
