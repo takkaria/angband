87909ae0c4ec1dc5fb602c7f76841fd034f25406
Vic K <t4nk074@openmailbox.org>
2016-09-15 11:38:39 +0800

    Use data->item_mode for quiver_tags

--- src/ui2-object.c
+++ src/ui2-object.c
@@ -1266,11 +1266,11 @@ static bool init_menu_data(struct object_menu_data *data,
 	data->f1 = f1;
 	data->f2 = f2;
 
-	const bool allow_inven  = (data->item_mode & USE_INVEN)  ? true : false;
-	const bool allow_equip  = (data->item_mode & USE_EQUIP)  ? true : false;
-	const bool allow_quiver = (data->item_mode & USE_FLOOR)  ? true : false;
-	const bool allow_floor  = (data->item_mode & USE_QUIVER) ? true : false;
-	const bool quiver_tags  = (mode & QUIVER_TAGS)           ? true : false;
+	const bool allow_inven  = (data->item_mode & USE_INVEN)   ? true : false;
+	const bool allow_equip  = (data->item_mode & USE_EQUIP)   ? true : false;
+	const bool allow_quiver = (data->item_mode & USE_FLOOR)   ? true : false;
+	const bool allow_floor  = (data->item_mode & USE_QUIVER)  ? true : false;
+	const bool quiver_tags  = (data->item_mode & QUIVER_TAGS) ? true : false;
 
 	/* Require at least one legal choice */
 	if (!allow_inven && !allow_equip && !allow_quiver && !allow_floor) {
