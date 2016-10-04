403c874704ad8eba0db45d26a7461d4e8e38bc2b
Vic K <t4nk074@openmailbox.org>
2016-10-04 09:27:09 +0800

    Change textui_get_quantity() prompt (again)
    "'a' for all" is nice and simple.

--- src/ui2-input.c
+++ src/ui2-input.c
@@ -525,7 +525,7 @@ int textui_get_quantity(const char *prompt, int max)
 
 		/* Build a prompt if needed */
 		if (prompt == NULL) {
-			strnfmt(tmp, sizeof(tmp), "Quantity (0-%d, any letter for all): ", max);
+			strnfmt(tmp, sizeof(tmp), "Quantity (0-%d, 'a' for all): ", max);
 			prompt = tmp;
 		}
 
