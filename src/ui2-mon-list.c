a706469817ab2d79226f32a0c5057a1d43f667e4
Vic K <t4nk074@openmailbox.org>
2016-10-05 19:37:54 +0800

    Fix comment

--- src/ui2-mon-list.c
+++ src/ui2-mon-list.c
@@ -485,8 +485,8 @@ void monster_list_show_subwindow(void)
 }
 
 /**
- * Display the monster list interactively. This will dynamically size the list
- * for the best appearance.
+ * Display the monster list interactively.
+ * This will dynamically size the list for the best appearance.
  */
 void monster_list_show_interactive(void)
 {
